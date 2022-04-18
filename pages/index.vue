7b35344c43a8aed4b5c2585dce09271f1dcd2846
Sriramula prathyusha <96115648+prathyushap628@users.noreply.github.com>
2022-04-18 18:10:28 +0530

    frontend

---
+++ pages/index.vue
@@ -0,0 +1,57 @@
+<template>
+  <div>
+    <LoginForm v-if="token == null" />
+
+    <div class="todo-list" v-if="todos.length > 0">
+    <div>
+    <input type="text" id="task" placeholder="Todo..." v-model="todo" />
+
+      <button @click="addTodo(todo)" class="btn add">ADD</button>
+
+    </div>
+
+    </div>
+
+      <TodoItem v-for="item in todos" :key="item.id" :item="item" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { mapState } from 'vuex'
+
+import LoginForm from '@/components/LoginForm.vue'
+import TodoItem from '@/components/Todo.vue'
+
+export default {
+  name: 'IndexPage',
+
+  components: {
+    LoginForm,
+    TodoItem,
+  },
+
+  computed: {
+    ...mapState(['token', 'todos']),
+  },
+   data() {
+        return {
+            todo: '',
+        }
+    },
+
+    methods: {
+
+     async   addTodo(todo) {
+          await  this.$store.dispatch('addTodo', todo);
+          await  this.$store.dispatch('getAllTodos');
+        },
+    }
+
+}
+</script>
+<style scoped>
+
+
+
+</style>
